c10b42e62c1a12ade499851c3cb1d29568e63966
Jonathan Damico <jj_damico@yahoo.com>
2016-03-10 14:20:47 -0800

    add s turns

--- autonomous.c
+++ autonomous.c
@@ -48,7 +48,8 @@ bool drivePID(int distance) {
 	return true;
 }
 
-bool sLeftPID(int distance, int coefficient) {
+#warning "sTurnPID"
+bool sTurnPID(int distance, int coefficient) {
 	nMotorEncoder[leftWheel13] = 0;
 	nMotorEncoder[rightWheel13] = 0;
 	pid straight;
@@ -64,55 +65,7 @@ bool sLeftPID(int distance, int coefficient) {
 
 	int timeGuess = 3*abs(distance);//#magic number 5
 	clearTimer(T2);
-	angle.target=0;
-	straight.target=distance;
-	do{
-		straight.error=straight.target -( nMotorEncoder[leftWheel13]+	nMotorEncoder[rightWheel13]);
-		angle.error=(nMotorEncoder[leftWheel13]-nMotorEncoder[rightWheel13]); //target is 0 so lazy
-
-
-		if(angle.error == 0) { angle.integral = 0; }
-		if(straight.error == 0) { straight.integral = 0; }
-
-		straight.derivative = straight.error - straight.lastError;
-		angle.derivative = angle.error - angle.lastError;
-
-		straight.lastError = straight.error;
-		angle.lastError = angle.error;
-
-		int StraightOut = straight.kP*straight.error + straight.kI*straight.integral + straight.kD*straight.derivative;
-		int AngleOut = angle.kP*angle.error + angle.kI*angle.integral + angle.kD*angle.derivative - coefficient;
-
-		clearLCD();
-		setLeftWheelSpeed(StraightOut+AngleOut);
-		setRightWheelSpeed(StraightOut-AngleOut);
-		delay(50);
-		if(time1[T2]>timeGuess){ //if something went wrong give up
-			setWheelSpeed(0);
-			return false;
-		}
-	}	while(abs(straight.error)>30 || abs(straight.lastError)>30);
-	setWheelSpeed(0);
-	return true;
-}
-
-bool sRightPID(int distance, int coefficient) {
-	nMotorEncoder[leftWheel13] = 0;
-	nMotorEncoder[rightWheel13] = 0;
-	pid straight;
-	pid angle;
-
-	straight.kP = 0.04;
-	straight.kI = 0.0001;
-	straight.kD = 0.04;
-
-	angle.kP = 0.02;//for
-	angle.kI = 0.0000;
-	angle.kD = 0//.15;
-
-	int timeGuess = 3*abs(distance);//#magic number 5
-	clearTimer(T2);
-	angle.target=0;
+	angle.target=coefficient;
 	straight.target=distance;
 	do{
 		straight.error=straight.target -( nMotorEncoder[leftWheel13]+	nMotorEncoder[rightWheel13]);
